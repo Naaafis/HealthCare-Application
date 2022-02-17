163351809ff4c1e0b26de85ab3a3f7299a1b0ecb
Nafis Abeer <cheffbcookin@dhcp-wifi-8021x-155-41-83-192.bu.edu>
2022-02-16 20:58:51 -0500

    removed leading zeros

--- devices_main.py
+++ devices_main.py
@@ -5,8 +5,8 @@ import codecs
 
 #this function exists to test that the data collection and device registration metrics work properly
 def test_register_device():
-    test_dict = {'u_id': 001, 'd_id': 001, 'd_type': "scale", 'd_mac': "00:00:11:11", 'd_firmware_v': 1.1.0, 'd_software_v': 1.1.0}
-    func_dict = rd.register_device(001, 001, "scale", "00:00:11:11", 1.1.0, 1.1.0)
+    test_dict = {'u_id': 1, 'd_id': 1, 'd_type': "scale", 'd_mac': "00:00:11:11", 'd_firmware_v': 1.1.0, 'd_software_v': 1.1.0}
+    func_dict = rd.register_device(1, 1, "scale", "00:00:11:11", 1.1.0, 1.1.0)
     assert test_dict['u_id'] == func_dict['u_id'], "Failed to register user ID"
     assert test_dict['d_id'] == func_dict['d_id'], "Failed to register device ID"
     assert test_dict['d_type'] == func_dict['d_type'], "Failed to register device type"
@@ -16,8 +16,8 @@ def test_register_device():
    
 #the functions arent testing for accuracy in time because it may differ by seconds
 def test_collect_data():
-    test_dict = {'p_id': 001, 'd_id': 001, d_type: "scale", d: 133}
-    func_dict = cd.collect_data(001, 001, "scale", 133)
+    test_dict = {'p_id': 1, 'd_id': 1, d_type: "scale", d: 133}
+    func_dict = cd.collect_data(1, 1, "scale", 133)
     assert test_dict['u_id'] == func_dict['u_id'], "Failed to collect user ID"
     assert test_dict['d_id'] == func_dict['d_id'], "Failed to collect device ID"
     assert test_dict['d_type'] == func_dict['d_type'], "Failed to collect device type"
@@ -25,7 +25,7 @@ def test_collect_data():
     
 #these functions exist to check that json collectin works properly
 def test_package_device_info():
-    func_dict = rd.register_device(001, 001, "scale", "00:00:11:11", 1.1.0, 1.1.0)
+    func_dict = rd.register_device(1, 1, "scale", "00:00:11:11", 1.1.0, 1.1.0)
     jsonpack = rd.package_device_info(func_dict)
     testjson = json.dumps(jsonpack)
     assert testjson['u_id'] == func_dict['u_id'], "Failed to register json user ID"
@@ -36,7 +36,7 @@ def test_package_device_info():
     assert testjson['d_software_v'] == func_dict['d_software_v'], "Failed to register json device software version"
     
 def test_package_data():
-    func_dict = cd.collect_data(001, 001, "scale", 133)
+    func_dict = cd.collect_data(1, 1, "scale", 133)
     jsonpack = cd.package_data(func_dict)
     testjson = json.dumps(jsonpack)
     assert testjson['u_id'] == func_dict['u_id'], "Failed to collect json user ID"
@@ -46,48 +46,48 @@ def test_package_data():
     
 #these functions exist to test the chcking functions 
 def test_check_registration():
-    func_dict = rd.register_device(001, 001, "scale", "00:00:11:11", 1.1.0, 1.1.0)
+    func_dict = rd.register_device(1, 1, "scale", "00:00:11:11", 1.1.0, 1.1.0)
     test = rd.check_registration(func_dict)
     assert test == True, "Failed to check registration"
-    func_dict = rd.register_device("001", 001, "scale", "00:00:11:11", 1.1.0, 1.1.0)
+    func_dict = rd.register_device("001", 1, "scale", "00:00:11:11", 1.1.0, 1.1.0)
     test = rd.check_registration(func_dict)
     assert test == False, "Failed to check registration"
-    func_dict = rd.register_device(001, "001", "scale", "00:00:11:11", 1.1.0, 1.1.0)
+    func_dict = rd.register_device(1, "001", "scale", "00:00:11:11", 1.1.0, 1.1.0)
     test = rd.check_registration(func_dict)
     assert test == False, "Failed to check registration"
-    func_dict = rd.register_device(001, 001, 133, "00:00:11:11", 1.1.0, 1.1.0)
+    func_dict = rd.register_device(1, 1, 133, "00:00:11:11", 1.1.0, 1.1.0)
     test = rd.check_registration(func_dict)
     assert test == False, "Failed to check registration"
-    func_dict = rd.register_device(001, 001, "dollar", "00:00:11:11", 1.1.0, 1.1.0)
+    func_dict = rd.register_device(1, 1, "dollar", "00:00:11:11", 1.1.0, 1.1.0)
     test = rd.check_registration(func_dict)
     assert test == False, "Failed to check registration"
-    func_dict = rd.register_device(001, 001, "scale", 00:00:11:11, 1.1.0, 1.1.0)
+    func_dict = rd.register_device(1, 1, "scale", 00:00:11:11, 1.1.0, 1.1.0)
     test = rd.check_registration(func_dict)
     assert test == False, "Failed to check registration"
-    func_dict = rd.register_device(001, 001, "scale", "00:00:11:11", "1.1.0", 1.1.0)
+    func_dict = rd.register_device(1, 1, "scale", "00:00:11:11", "1.1.0", 1.1.0)
     test = rd.check_registration(func_dict)
     assert test == False, "Failed to check registration"
-    func_dict = rd.register_device(001, 001, "scale", "00:00:11:11", 1.1.0, "1.1.0")
+    func_dict = rd.register_device(1, 1, "scale", "00:00:11:11", 1.1.0, "1.1.0")
     test = rd.check_registration(func_dict)
     assert test == False, "Failed to check registration"
     
 def test_check_collected_data():
-    func_dict = cd.collect_data(001, 001, "scale", 133)
+    func_dict = cd.collect_data(1, 1, "scale", 133)
     test = cd.check_collected_data(func_dict)
     assert test == True, "Failed to check collected data"
-    func_dict = cd.collect_data("001", 001, "scale", 133)
+    func_dict = cd.collect_data("001", 1, "scale", 133)
     test = cd.check_collected_data(func_dict)
     assert test == False, "Failed to check collected data"
-    func_dict = cd.collect_data(001, "001", "scale", 133)
+    func_dict = cd.collect_data(1, "001", "scale", 133)
     test = cd.check_collected_data(func_dict)
     assert test == False, "Failed to check collected data"
-    func_dict = cd.collect_data(001, 001, "haha", 133)
+    func_dict = cd.collect_data(1, 1, "haha", 133)
     test = cd.check_collected_data(func_dict)
     assert test == False, "Failed to check collected data"
-    func_dict = cd.collect_data(001, 001, 133, 133)
+    func_dict = cd.collect_data(1, 1, 133, 133)
     test = cd.check_collected_data(func_dict)
     assert test == False, "Failed to check collected data"
-    func_dict = cd.collect_data(001, 001, "scale", "133")
+    func_dict = cd.collect_data(1, 1, "scale", "133")
     test = cd.check_collected_data(func_dict)
     assert test == False, "Failed to check collected data"
     
