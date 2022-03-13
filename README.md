13c098dd0f91cdddba6ddc38cf8c980f1190673e
Nafis Abeer <cheffbcookin@brittnys-air.myfiosgateway.com>
2022-03-13 19:08:28 -0400

    added functionality for local mongoDB server and local API to query server

--- README.md
+++ README.md
@@ -63,3 +63,37 @@ Chat Tables:
 - Messages and message IDs 
 
 
+------------------------------------------------------------------------------------------------
+ChatAPI use instrucitons:
+
+To deploy API, cd into chat-flask-project and run the following commands:
+pip install pipenv
+pipenv --three
+pipenv install flask
+./bootstrap.sh
+
+take note of the http link running the API
+
+use POST calls along with the following parameters for json requests to send messages:
+{sender: *string*, recipient: *string*, message: *string*}
+
+------------------------------------------------------------------------------------------------
+Database for Chat decision purposes:
+
+- Need something lightweight to be able to constantly update the database when new messages are received
+- Must follow all of ACID properties
+- Dont really need PRIMARY and FOREIGN keys as there is no need for object relational mapping when messages are only relevant to one key
+- Need the CR part of CRUD operations to be able to create and retrieve messages, might even need to update and delete messages 
+- The Key would simply be a ID created for the tuple of the sender and recipient pair 
+- Just need one collection called messages, it'll hold each document as messaging sessions between two users
+- Easy to scale documents by nesting it with new messages
+- Hence I will be using MongoDB
+
+------------------------------------------------------------------------------------------------
+To use mongoDB: 
+Run the following commands:
+
+On MacOS:
+brew services start mongodb-community@5.0
+mongosh
+
--- chat-flask-project/chatAPI/index.py
+++ chat-flask-project/chatAPI/index.py
@@ -1,7 +1,21 @@
 import json
+from bson import json_util
+import pymongo
+from pymongo import MongoClient #Needed for compatibility with MongoDB
+# pprint library is used to make the output look more pretty
+from pprint import pprint
 
 from flask import Flask, jsonify, request
 app = Flask(__name__)
+
+########################################################################
+#connect to mongo client 
+client = MongoClient("mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.2.3")
+#currently the client link refers to localhost mongodb
+db = client["test"]
+col = db["messages"]
+
+########################################################################
 
 message_collection = [
     
@@ -31,13 +45,21 @@ def check_message(inp):
 
 @app.route("/messages", methods=["GET"])
 def get_all_messages():
-    return jsonify(message_collection)
+    users_id = request.get_json()
+    users_id = ''.join(sorted(users_id['sender']+users_id['recipient']))
+    result = col.find({"users": users_id})
+    result = json.loads(json_util.dumps(result))
+    #return jsonify(message_collection)
+    return jsonify(result)
 
 @app.route("/messages", methods=['POST'])
 def add_device():
     new_message = request.get_json()
     if check_message(new_message) == "Valid Message":
         message_collection.append(request.get_json())
+        inserting_m = {'users': ''.join(sorted(new_message['sender']+new_message['recipient'])), 'sender': new_message['sender'], 'recipient': new_message['recipient'], 'message': new_message['message']}
+        result = col.insert_one(inserting_m)
+        print('Created {0}'.format(result.inserted_id))
         return check_message(new_message)
     else: 
         return check_message(new_message)
