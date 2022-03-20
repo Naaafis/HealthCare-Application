97705d4dd011d8d0ebfd7e6455235de76fcd03dc
Nafis Abeer <cheffbcookin@dhcp-wifi-8021x-155-41-26-100.bu.edu>
2022-03-20 18:32:43 -0400

    added threading flask api

--- threading-flask/threads/index.py
+++ threading-flask/threads/index.py
@@ -1,8 +1,40 @@
-from flask import Flask
+#Source: https://pythonise.com/series/learning-flask/flask-rq-task-queue
+
+from flask import Flask, request
+import redis 
+from rq import Queue
+
+import time
+
 app = Flask(__name__)
 
+r = redis.Redis()
+q = Queue(connection = r)
 
-@app.route("/")
-def hello_world():
-  return "Hello, World!"
+def background_task(n):
+
+    """ Function that returns len(n) and simulates a delay """
+
+    delay = 2
+
+    print("Task running")
+    print(f"Simulating a {delay} second delay")
+
+    time.sleep(delay)
+
+    print(len(n))
+    print("Task complete")
+
+    return len(n)
+
+@app.route("/task")
+def index():
+
+    if request.args.get("n"):
+
+        job = q.enqueue(background_task, request.args.get("n"))
+
+        return f"Task ({job.id}) added to queue at {job.enqueued_at}"
+
+    return "No value for count provided"
 
